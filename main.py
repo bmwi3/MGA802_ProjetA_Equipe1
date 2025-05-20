289e8e84a49841ca44d24749bc116afac78969ff
bmwi3 <julien.giner@utbm.fr>
2025-05-20 15:09:40 -0400

    Ajout des nom principaux des variables

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #Code principal à ouvrir par l'utilisateur
+# Décalage de l'encodage
+cle=0
+
+#Text console à décoder // Fichier
+texte_encode=[]
+
+#Texte console a encode //Fichier encodé
+texte_decode=[]
+
+#brute force
+texte_devine=[]
+cle_devine=[]
+
+#Input utilisateur
+texte_entre='str'
+
+#Chemin
+chemin_fichier=''
+
+
+
+
