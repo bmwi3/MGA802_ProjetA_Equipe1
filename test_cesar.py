32a0f7bdd3e4ab71a26afb584d2fed949f5604e9
bmwi3 <julien.giner@utbm.fr>
2025-05-20 14:10:06 -0400

    Add brute force test

--- test_cesar.py
+++ test_cesar.py
@@ -1,2 +1,32 @@
-if __name__=="__main__":
-   
+def encode_cesar(text, shift):
+    """
+    Encodes the given text using Caesar cipher with the specified shift.
+    """
+    encoded_text = ""
+    for char in text:
+        if char.isalpha():
+            shift_base = ord('A') if char.isupper() else ord('a')
+            encoded_text += chr((ord(char) - shift_base + shift) % 26 + shift_base)
+        else:
+            encoded_text += char
+    return encoded_text
+
+def decode_cesar(text, shift):
+    """
+    Decodes the given text using Caesar cipher with the specified shift.
+    """
+    return encode_cesar(text, -shift)
+
+def brute_force_cesar(text):
+    """
+    Attempts to decode the given text using all possible shifts (0-25) and returns the best guess.
+    """
+    best_guess = ""
+    best_score = 0
+    for shift in range(26):
+        decoded_text = decode_cesar(text, shift)
+        score = sum(1 for char in decoded_text if char.isalpha())
+        if score > best_score:
+            best_score = score
+            best_guess = decoded_text
+    return best_guess
